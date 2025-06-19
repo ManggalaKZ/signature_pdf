be65596ac119bb9b6eb311ffafba2c424835e9a0
Manggala Kagendra Zuhdi <kagendra147@gmail.com>
2025-06-19 13:30:19 +0700

    Refactor code structure for improved readability and maintainability

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,73 +1,85 @@
 import 'dart:io';
-import 'dart:typed_data';
 import 'dart:math' as m;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:signature_pdf/app/modules/home/views/signature_pdf_preview.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
-import 'package:flutter/gestures.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
-import 'package:vector_math/vector_math_64.dart';
 
 class HomeController extends GetxController {
-  final String assetPath = 'assets/contohpdf.pdf';
+  final String assetPath = 'assets/laporanAM.pdf';
 
   RxBool isSigningMode = false.obs;
   Rx<Offset?> tappedPosition = Rx<Offset?>(null);
   Rx<Uint8List?> signatureImage = Rx<Uint8List?>(null);
   late Size pdfViewSize;
+  final currentPageNumber = 1.obs;
 
   final pdfViewerController = PdfViewerController();
   final TransformationController pdfTransformController =
       TransformationController();
 
+  @override
+  void onInit() {
+    super.onInit();
+    pdfViewerController.addListener(() {
+      currentPageNumber.value = pdfViewerController.pageNumber;
+    });
+  }
+
   Future<void> addSignature({
     required Size pdfViewSize,
+    required int pageNumber,
   }) async {
     if (tappedPosition.value == null || signatureImage.value == null) return;
 
     final ByteData data = await rootBundle.load(assetPath);
     final Uint8List bytes = data.buffer.asUint8List();
     final PdfDocument document = PdfDocument(inputBytes: bytes);
-    final Offset tapOffset = tappedPosition.value!;
-    final PdfPage page = document.pages[0];
-    final Size pageSize = page.size;
 
-    final double scaleX = pdfViewSize.width / pageSize.width;
-    final double displayedPageHeight = pageSize.height * scaleX;
+    final Offset tapOffset = tappedPosition.value!;
+    final int pageCount = document.pages.count;
+    final Size firstPageSize = document.pages[0].size;
+
+    final double scaleX = pdfViewSize.width / firstPageSize.width;
+    final double displayedPageHeight = firstPageSize.height * scaleX;
     final double verticalMargin =
         (pdfViewSize.height - displayedPageHeight) / 2;
-    final double correctedScreenY = tapOffset.dy - verticalMargin;
-    
+    final double totalHeightPerPage =
+        displayedPageHeight + (verticalMargin * 2);
+
+    // 👇 Hitung halaman berdasarkan posisi ketukan Y
+    final int tappedPageIndex =
+        (tapOffset.dy / totalHeightPerPage).floor().clamp(0, pageCount - 1);
+    print("Menambahkan TTD ke halaman sebelum di $pageNumber");
+    final PdfPage page = document.pages[pageNumber - 1];
+     print("Menambahkan TTD ke halaman $pageNumber");
+    final Size pageSize = page.size;
+
+    final double correctedScreenY =
+        tapOffset.dy - (tappedPageIndex * totalHeightPerPage) - verticalMargin;
     final double clampedScreenY =
         correctedScreenY.clamp(0.0, displayedPageHeight);
     final double pdfX = tapOffset.dx / scaleX;
     final double pdfYFromTop = clampedScreenY / scaleX;
 
-    print("DEBUG: Posisi tap di layar: $tapOffset");
-    print("DEBUG: Ukuran viewer di layar: $pdfViewSize");
+    print("DEBUG: Tap di layar: $tapOffset");
+    print("DEBUG: Tap dikenali di halaman ke: ${tappedPageIndex }");
+    print("DEBUG: Ukuran viewer: $pdfViewSize");
     print("DEBUG: Ukuran halaman PDF: $pageSize");
-    print("DEBUG: scaleX: $scaleX");
-    print("DEBUG: verticalMargin: $verticalMargin");
-    print("DEBUG: correctedScreenY: $correctedScreenY");
-    print("DEBUG: clampedScreenY: $clampedScreenY");
     print("DEBUG: Posisi X di PDF (dari kiri): $pdfX");
     print("DEBUG: Posisi Y di PDF (dari atas): $pdfYFromTop");
 
     final PdfBitmap bitmap = PdfBitmap(signatureImage.value!);
-    const double targetWidth = 120; 
+    const double targetWidth = 100;
     final double aspectRatio = bitmap.height / bitmap.width;
     final double targetHeight = targetWidth * aspectRatio;
 
-    final double finalPdfYForDrawing =
-        pageSize.height - pdfYFromTop - targetHeight;
-
     page.graphics.drawImage(
       bitmap,
-      Rect.fromLTWH(pdfX, finalPdfYForDrawing, targetWidth, targetHeight),
+      Rect.fromLTWH(pdfX - 20, pdfYFromTop - 20, targetWidth, targetHeight),
     );
 
     final List<int> outputBytes = await document.save();
@@ -82,4 +94,26 @@ class HomeController extends GetxController {
       transition: Transition.rightToLeft,
     );
   }
+
+  // // Tambahkan method ini di controller
+  // int getTappedPage(Offset tapPositionInViewer) {
+  //   final zoom = pdfViewerController.zoomLevel;
+  //   final viewerSize = pdfViewSize;
+  //   final pageOffsets = pdfViewerController.pageOffsets;
+
+  //   double y = tapPositionInViewer.dy + pdfViewerController.scrollOffset.dy;
+
+  //   for (int i = 0; i < pageOffsets.length; i++) {
+  //     final pageTop = pageOffsets[i];
+  //     final pageBottom =
+  //         pageTop + pdfViewerController.pagesize[i].height * zoom;
+
+  //     if (y >= pageTop && y <= pageBottom) {
+  //       return i +
+  //           1; // karena index mulai dari 0, sedangkan halaman mulai dari 1
+  //     }
+  //   }
+
+  //   return 1; // fallback default halaman 1
+  // }
 }
--- lib/app/modules/home/views/home_views.dart
+++ lib/app/modules/home/views/home_views.dart
@@ -1,74 +1,58 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:signature_pdf/app/modules/home/widgets/signature_draw_dialog.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import '../../../controllers/home_controller.dart';
-import 'package:vector_math/vector_math_64.dart' as vm;
 
 class HomeView extends GetView<HomeController> {
   final GlobalKey<SfPdfViewerState> pdfViewerKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Signature PDF')),
-      body: Stack(
-        children: [
-          InteractiveViewer(
-              transformationController: controller.pdfTransformController,
-              minScale: 1.0,
-              maxScale: 5.0,
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  controller.pdfViewSize =
-                      Size(constraints.maxWidth, constraints.maxHeight);
-                  return Container(
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight,
-                    child: SfPdfViewer.asset(
-                      'assets/contohpdf.pdf',
-                      controller: controller.pdfViewerController,
-                    ),
-                  );
-                },
-              )),
-          Positioned.fill(
-            child: GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTapUp: (TapUpDetails details) async {
-                final tapPos = details.localPosition;
-                print("DEBUG: User tap position (localPosition): $tapPos");
-                if (controller.isSigningMode.value) {
-                  final Matrix4 inverse =
-                      Matrix4.inverted(controller.pdfTransformController.value);
-                  final vector = inverse.transform3(vm.Vector3(
-                      details.localPosition.dx, details.localPosition.dy, 0));
-                  final transformedTap = Offset(vector.x, vector.y);
-
-                  controller.tappedPosition.value = transformedTap;
-                  print(
-                      "DEBUG: Tap position after inverse transform: $transformedTap");
-
-                  controller.isSigningMode.value = false;
-
-                  final result = await showDialog<Uint8List>(
-                    context: context,
-                    builder: (_) => const SignatureDrawDialog(),
-                  );
-
-                  if (result != null) {
-                    controller.signatureImage.value = result;
-                    await controller.addSignature(
-                        pdfViewSize: controller.pdfViewSize);
-                  }
-                }
+      body: Obx(() {
+        return Stack(
+          children: [
+            LayoutBuilder(
+              builder: (context, constraints) {
+                controller.pdfViewSize =
+                    Size(constraints.maxWidth, constraints.maxHeight);
+                return SfPdfViewer.asset(
+                  'assets/laporanAM.pdf',
+                  key: pdfViewerKey,
+                  controller: controller.pdfViewerController,
+                  pageLayoutMode: PdfPageLayoutMode.single,
+                  scrollDirection: PdfScrollDirection.vertical,
+                );
               },
-              child: Container(color: Colors.transparent),
             ),
-          ),
-          Obx(() => controller.isSigningMode.value
-              ? IgnorePointer(
+            if (controller.isSigningMode.value)
+              Positioned.fill(
+                child: GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTapUp: (TapUpDetails details) async {
+                    final currentPage = controller.currentPageNumber.value;
+                    print("DEBUG: Current page number: $currentPage");
+                    final tapPos = details.localPosition;
+                    print("DEBUG: Tap position: $tapPos");
+
+                    controller.tappedPosition.value = tapPos;
+                    controller.isSigningMode.value = false;
+
+                    final result = await showDialog<Uint8List>(
+                      context: context,
+                      builder: (_) => const SignatureDrawDialog(),
+                    );
+
+                    if (result != null) {
+                      controller.signatureImage.value = result;
+                      await controller.addSignature(
+                          pageNumber: currentPage,
+                          pdfViewSize: controller.pdfViewSize);
+                    }
+                  },
                   child: Container(
                     color: Colors.black.withOpacity(0.3),
                     alignment: Alignment.center,
@@ -77,10 +61,11 @@ class HomeView extends GetView<HomeController> {
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                   ),
-                )
-              : const SizedBox.shrink()),
-        ],
-      ),
+                ),
+              ),
+          ],
+        );
+      }),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           controller.isSigningMode.value = true;
